7a0980d923636bae0560a9c81034f6f51e6cf3a5
Denis Tatarinov <dengertd@gmail.com>
2021-01-29 16:33:04 +0500

    all modify

---
+++ src/BonusService.java
@@ -0,0 +1,5 @@
+public class BonusService {
+    public long calculate (long amount, boolean registered) {
+        return 0;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -82,3 +82,49 @@
     }
 }*/
 
+/*public class Main {
+
+    public static void main(String[] args) {
+        boolean registered = true;
+        int percent = registered ? 3 : 1;//сокращение if - else
+        long amount = 100060;
+        long bonus = amount * percent / 100 / 100;
+        int limit = 500;
+
+        if (bonus > limit) {
+            bonus = limit;
+        }
+        System.out.println(bonus);
+
+    }
+}*/
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
